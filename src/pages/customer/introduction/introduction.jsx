ae458eef0dec4c4ed30f1f9e0984b7617c1dbfff
Huy <Huyphan1232203@gmail.com>
2024-10-03 14:00:20 +0700

    introduction

--- src/pages/customer/introduction/introduction.jsx
+++ src/pages/customer/introduction/introduction.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./introduction.css";
 function Introduction() {
   return (
     <div className="Introduction">
-      <div className="Intro_header"></div>
+      <div className="Intro_header">asdm</div>
       <div className="Intro_body">adsaoidnio</div>
     </div>
   );
