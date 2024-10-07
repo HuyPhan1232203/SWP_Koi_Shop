931941b8b4fcb33d5175dbab00d97c77413bf19f
Huy <Huyphan1232203@gmail.com>
2024-10-07 16:16:46 +0700

    fetch koi user

--- src/pages/user/koi_breed_type/koi_breed_type.jsx
+++ src/pages/user/koi_breed_type/koi_breed_type.jsx
@@ -1,7 +1,41 @@
-import React from "react";
-import "./koi_breed-type.css";
+import React, { useEffect, useState } from "react";
+import "./koi_breed-type.scss";
+import { useDispatch } from "react-redux";
+import { Button } from "antd";
+import api from "../../../config/axios";
+import { addProduct } from "../../../redux/features/cartSlice";
 function KoiBreedType() {
-  return <div className="KoiBreedType"></div>;
-}
+  const [products, setProduct] = useState([]);
 
+  const fetchProduct = async () => {
+    const response = await api.get("koi");
+    setProduct(response.data.content);
+    console.log(response.data.content);
+  };
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  // return (
+  //   <div className="koi_list">
+  //     {products.map((product) => (
+  //       <Product product={product} />
+  //     ))}
+  //   </div>
+  // );
+}
+// const Product = (product) => {
+//   const dispatch = useDispatch();
+//   const handleAddToCart = () => {
+//     dispatch(addProduct(product));
+//   };
+//   return (
+//     <div className="product">
+//       <img src="" alt="" />
+//       <p>product.name</p>
+//       <p>product.name</p>
+//       <p>product.name</p>
+//       <Button onClick={handleAddToCart()}>Add</Button>
+//     </div>
+//   );
+// };
 export default KoiBreedType;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "../redux/features/userSlice"
+import cartReducer from "../redux/features/cartSlice"
+
 export const rootReducer = combineReducers({
-    user:userReducer
+    user:userReducer,
+    cart:cartReducer
 });
