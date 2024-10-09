566b0818711a1ad0b593fc2ec8ed91a9dbb294f8
Huy <Huyphan1232203@gmail.com>
2024-10-09 12:02:41 +0700

    koilot

--- src/pages/manager/manage-koi-lot/manage-koi-lot.jsx
+++ src/pages/manager/manage-koi-lot/manage-koi-lot.jsx
@@ -1,160 +1,310 @@
-import React from "react";
-import CRUDTemplate from "../../../conponent/crud-template/crud-template";
-import { Form, Input, Radio } from "antd";
-
-function ManageKoiLot() {
-  const columns = [
+import {
+  Button,
+  Form,
+  Image,
+  Input,
+  InputNumber,
+  Modal,
+  Popconfirm,
+  Radio,
+  Select,
+  Table,
+  Upload,
+} from "antd";
+// import "./ManagementKoi.css";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import api from "../../../config/axios";
+const ManagementKoi = () => {
+  const [KoiFish, setKoiFish] = useState([]);
+  const [formStand] = Form.useForm();
+  const [submitKoi, setSubmitKoi] = useState(false);
+  const [submitBreed, setSubmitBreed] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  //FETCH
+  const fetchKoi = async () => {
+    try {
+      const response = await api.get("koilot?page=0&size=20");
+      setKoiFish(response.data.content);
+    } catch (err) {
+      toast.error(err.response.data);
+    }
+  };
+  const fetchBreed = async () => {
+    try {
+      const response = await api.get("breed");
+      setSubmitBreed(response.data);
+      console.log(response.data);
+    } catch (err) {
+      toast.error(err.response.data);
+    }
+  };
+  //USE EFFECT
+  useEffect(() => {
+    fetchKoi();
+    fetchBreed();
+  }, []);
+  //OPEN MODAL
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+  //CLOSE MODEL
+  const handleClosenModal = () => {
+    setOpenModal(false);
+  };
+  //COLUMNS
+  const cols = [
+    // {
+    //   title: "Image",
+    //   dataIndex: "image",
+    //   key: "image",
+    //   render: (image) => {
+    //     return <Image src={image} alt="" width={200} />;
+    //   },
+    // },
+    {
+      title: "id",
+      dataIndex: "id",
+      key: "id",
+    },
     {
       title: "Name",
       dataIndex: "name",
       key: "name",
     },
     {
-      title: "Breed Id",
-      dataIndex: "breedId",
-      key: "breedId",
-    },
-    {
-      title: "Price",
+      title: "price",
       dataIndex: "price",
       key: "price",
     },
     {
-      title: "Vendor",
+      title: "vendor",
       dataIndex: "vendor",
       key: "vendor",
     },
     {
-      title: "Gender",
+      title: "gender",
       dataIndex: "gender",
       key: "gender",
     },
     {
-      title: "Born Year",
+      title: "bornYear",
       dataIndex: "bornYear",
       key: "bornYear",
     },
     {
-      title: "Size",
+      title: "size",
       dataIndex: "size",
       key: "size",
     },
     {
-      title: "Origin",
+      title: "breed",
+      dataIndex: "breedName",
+      key: "breedName",
+    },
+
+    {
+      title: "origin",
       dataIndex: "origin",
       key: "origin",
     },
     {
-      title: "Description",
+      title: "description",
       dataIndex: "description",
       key: "description",
     },
     {
-      title: "Quantity",
+      title: "quantity",
       dataIndex: "quantity",
       key: "quantity",
     },
     {
-      title: "Images",
-      dataIndex: "images",
-      key: "images",
+      title: "Action",
+      dataIndex: "id",
+      key: "id",
+      render: (id, koi) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              width: "150px",
+              justifyContent: "space-around",
+            }}
+          >
+            <Button
+              type="primary"
+              onClick={() => {
+                setOpenModal(true);
+                formStand.setFieldsValue(koi);
+              }}
+            >
+              Edit
+            </Button>
+
+            <Popconfirm
+              onConfirm={() => {
+                handleDeleteKoi(id);
+              }}
+              title="Delete"
+              description="Are you sure"
+            >
+              <Button type="primary" danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          </div>
+        );
+      },
     },
   ];
-
-  const formItems = (
-    <>
-      <Form.Item
-        label="Name"
-        rules={[{ required: true, message: "Please Input Breed's Name" }]}
-        name="name"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Breed ID"
-        rules={[{ required: true, message: "Please Input Breed's ID" }]}
-        name="breedId"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Price"
-        rules={[{ required: true, message: "Please Input Price" }]}
-        name="Price"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Vendor"
-        rules={[{ required: true, message: "Please Input Vendor" }]}
-        name="vendor"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Gender"
-        rules={[{ required: true, message: "Please Input Gender" }]}
-        name="gender"
-      >
-        <Radio.Group name="radiogroup">
-          <Radio value="Male">Male</Radio>
-          <Radio value="Female">Female</Radio>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item
-        label="Born Year"
-        rules={[{ required: true, message: "Please Input Born Year" }]}
-        name="bornYear"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Size"
-        rules={[{ required: true, message: "Please Input Size" }]}
-        name="size"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Origin"
-        rules={[{ required: true, message: "Please Input Origin" }]}
-        name="origin"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Description"
-        rules={[{ required: true, message: "Please Input Description" }]}
-        name="description"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Quantity"
-        rules={[{ required: true, message: "Please Input Quantity" }]}
-        name="quantity"
-      >
-        <Input></Input>
-      </Form.Item>
-      <Form.Item
-        label="Images"
-        rules={[{ required: true, message: "Please Input Images" }]}
-        name="images"
-      >
-        <Input></Input>
-      </Form.Item>
-    </>
-  );
+  //DELETE
+  const handleDeleteKoi = async (id) => {
+    try {
+      await api.delete(`koi/${id}`);
+      toast.success("Deleteted successfully!!!");
+      fetchKoi();
+    } catch (err) {
+      toast.error(err);
+    }
+  };
+  //CREATE OR UPDATE
+  const handleSubmitKoi = async (Koi) => {
+    try {
+      setSubmitKoi(true);
+      if (Koi.id) {
+        //update
+        const response = await api.put(`koilot/${Koi.id}`, Koi);
+        console.log(response);
+      } else {
+        //create
+        const response = await api.post("koilot", Koi);
+        console.log(Koi);
+      }
+      fetchKoi();
+      toast.success("Update successfully!!!");
+      formStand.resetFields();
+      handleClosenModal();
+    } catch (err) {
+      console.log("error");
+    } finally {
+      setSubmitKoi(false);
+    }
+  };
+  //SHOW BREED LIST
+  // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
   return (
     <div>
-      <CRUDTemplate
-        columns={columns}
-        formItems={formItems}
-        name="Koi Lot"
-        apiName="koilot"
-      />
+      <div className="header text-center p-5 ">
+        <h1 className="header_text" style={{ color: "#FE7A36" }}>
+          Koi Management
+        </h1>
+      </div>
+      <Button className="add_btn" onClick={handleOpenModal}>
+        Create Koi
+      </Button>
+      <Modal
+        confirmLoading={submitKoi}
+        title={<span className="Modal_header">KOI INFORMATION</span>}
+        open={openModal}
+        onCancel={() => {
+          handleClosenModal();
+          formStand.resetFields();
+        }}
+        onOk={formStand.submit}
+      >
+        <Form onFinish={handleSubmitKoi} form={formStand}>
+          <Form.Item hidden label="id" name="id">
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="Name"
+            rules={[{ required: true, message: "Please Input" }]}
+            name="name"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="price"
+            rules={[
+              { required: true, message: "Please Input" },
+              // { min: 1000, message: "Price cannot lower than 1000" },
+            ]}
+            name="price"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="vendor"
+            rules={[{ required: true, message: "Please Input" }]}
+            name="vendor"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="gender"
+            rules={[{ required: true, message: "Please Input" }]}
+            name="gender"
+          >
+            <Radio.Group name="radiogroup">
+              <Radio value="Male">Male</Radio>
+              <Radio value="Female">Female</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            label="bornYear"
+            rules={[{ required: true, message: "Please Input" }]}
+            name="bornYear"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item label="breedName" name="breedId">
+            <Select>
+              {submitBreed.map((breed) => (
+                <Select.Option key={breed.name} breed={breed} value={breed.id}>
+                  {breed.name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="description"
+            rules={[{ required: true, message: "Please Input" }]}
+            name="description"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="quantity"
+            rules={[{ required: true, message: "Please Input" }]}
+            name="quantity"
+          >
+            <InputNumber></InputNumber>
+          </Form.Item>
+
+          <Form.Item
+            label="origin"
+            rules={[{ required: true, message: "Please Input" }]}
+            name="origin"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            label="size"
+            rules={[
+              { required: true, message: "Please Input" },
+              {
+                type: "number",
+                message: "Invalid Input",
+              },
+            ]}
+            name="size"
+          >
+            <InputNumber></InputNumber>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Table columns={cols} dataSource={KoiFish}></Table>
     </div>
   );
-}
-
-export default ManageKoiLot;
+};
+export default ManagementKoi;
