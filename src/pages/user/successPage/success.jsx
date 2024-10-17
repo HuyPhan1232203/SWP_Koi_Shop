a0d0cae485591d60b3988f57c847311b473d9a49
TuonggVy <hongoctuongvy261104@gmail.com>
2024-10-17 11:05:06 +0700

    success

--- src/pages/user/successPage/success.jsx
+++ src/pages/user/successPage/success.jsx
@@ -33,7 +33,7 @@ function Success() {
     <div>
       <Result
         status="success"
-        title="Successfully Purchased Cloud Server ECS!"
+        title="Successfully Purchased!"
         subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
         extra={[
           <Link to="/">
