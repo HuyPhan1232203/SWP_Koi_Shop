7510f5f78def635af6c98919a6ab887c654b8ab2
TuonggVy <hongoctuongvy261104@gmail.com>
2024-10-17 11:01:05 +0700

    success

--- src/pages/user/successPage/success.jsx
+++ src/pages/user/successPage/success.jsx
@@ -1,8 +1,33 @@
 import { Button, Result } from "antd";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import useGetParams from "../../hooks/useGetParam";
 
 function Success() {
+  const nav = useNavigate();
+
+  const params = useGetParams();
+  const orderID = params("orderID");
+  const vnp_TransactionStatus = params("vnp_TransactionStatus");
+  console.log("orderID: ", orderID);
+  console.log("vnp_TransactionStatus: ", vnp_TransactionStatus);
+
+  const postOrderID = async () => {
+    try {
+      const response = await api.post(`/order/transaction?orderID=${orderID}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (vnp_TransactionStatus === "00") {
+      postOrderID();
+    } else {
+      // Failed
+      nav("/");
+    }
+  }, []);
 
   return (
     <div>
@@ -12,10 +37,10 @@ function Success() {
         subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
         extra={[
           <Link to="/">
-          <Button type="primary" key="console">
-            Go Home Page
-          </Button>
-          </Link>
+            <Button type="primary" key="console">
+              Go Home Page
+            </Button>
+          </Link>,
         ]}
       />
     </div>
