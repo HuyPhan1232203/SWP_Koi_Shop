7987e9adcc9c612c19f5831f8edff9d117b3e9af
Huy <Huyphan1232203@gmail.com>
2024-10-18 19:18:42 +0700

    import api

--- src/pages/user/successPage/success.jsx
+++ src/pages/user/successPage/success.jsx
@@ -2,9 +2,10 @@ import { Button, Result } from "antd";
 import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useGetParams from "../../hooks/useGetParam";
-import "./success.css"
+import "./success.css";
+import api from "../../../config/axios";
 
-function  Success() {
+function Success() {
   const nav = useNavigate();
 
   const params = useGetParams();
@@ -31,15 +32,21 @@ function  Success() {
   }, []);
 
   return (
-    <div className="success-section"> 
+    <div className="success-section">
       <h2>KOI SHOP</h2>
       <p>Thank you for your payment</p>
       <div className="success-text">
-        <img className="success-img" src="https://icons.veryicon.com/png/o/miscellaneous/collect-and-receive-cash/successful-payment-3.png"></img>
+        <img
+          className="success-img"
+          src="https://icons.veryicon.com/png/o/miscellaneous/collect-and-receive-cash/successful-payment-3.png"
+        ></img>
         <p>Payment Successful</p>
-        <p className="sub-text">"Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait.</p>
+        <p className="sub-text">
+          "Order number: 2017182818828182881 Cloud server configuration takes
+          1-5 minutes, please wait.
+        </p>
         <Link to="/">
-        <button>Go To Home Page</button>
+          <button>Go To Home Page</button>
         </Link>
       </div>
     </div>
