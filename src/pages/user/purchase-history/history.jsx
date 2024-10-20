9308dbc6d87e97c9058fa42bd42da71d0f7574e9
Huy <Huyphan1232203@gmail.com>
2024-10-20 15:01:11 +0700

    history desgin

--- src/pages/user/purchase-history/history.jsx
+++ src/pages/user/purchase-history/history.jsx
@@ -1,48 +1,53 @@
-import { Table } from "antd";
-import React, { useEffect, useState } from "react";
+import { Button } from "antd";
+import { useEffect, useState } from "react";
 import api from "../../../config/axios";
-
+import "./history.css";
 function History() {
   const [orderList, setOrderList] = useState([]);
   const fetchOrder = async () => {
-    const response = await api.get("order");
+    const response = await api.get("order/customer");
     setOrderList(response.data);
     console.log(orderList);
   };
   useEffect(() => {
     fetchOrder();
   }, []);
-  const columns = [
-    {
-      title: "Date",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "Total",
-      dataIndex: "total",
-      key: "total",
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-      key: "status",
-      render: (status) => {
-        if (status === "PENDING") {
-          return <div style={{ color: "red" }}>{status}</div>;
-        } else {
-          return <div style={{ color: "green" }}>{status}</div>;
-        }
-      },
-    },
-  ];
   return (
-    <div>
-      <Table
-        columns={columns}
-        dataSource={orderList}
-        pagination={{ pageSize: 7 }}
-      ></Table>
+    <div className="history" style={{ height: "500px", overflowY: "auto" }}>
+      {orderList.map((order) => {
+        return (
+          <div key={order.id} className="order">
+            {order.orderDetails.map((koi) => (
+              <div key={koi.id} className="koi_detail row">
+                <img
+                  src={koi.imageUrl}
+                  className="col-md-2"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+                <div
+                  style={{ display: "flex", flexDirection: "column" }}
+                  className="col-md-8"
+                >
+                  <div style={{ fontWeight: "600", fontSize: "20px" }}>
+                    {koi.koiName}
+                  </div>
+                  <small>description: {order?.description}</small>
+                </div>
+                <div className="col-md-2 koiPrice">${koi.price}</div>
+              </div>
+            ))}
+            <div className="total">
+              Total: <p style={{ color: "green" }}>${order.total}</p>
+            </div>
+            <div className="buttons">
+              <Button className="bton">Rating</Button>
+              <Button>Connect to seller</Button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
