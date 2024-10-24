c1aabff2c2ddeae908f3751446d38538ee8d8357
Huy <Huyphan1232203@gmail.com>
2024-10-24 12:56:41 +0700

    consngin staff

--- src/pages/staff/manage-consign-care/manage-consign-care.jsx
+++ src/pages/staff/manage-consign-care/manage-consign-care.jsx
@@ -1,13 +1,28 @@
 import React, { useEffect, useState } from "react";
 import CRUDTemplate from "../../../conponent/crud-template/crud-template";
-import { Image, Table } from "antd";
+import { Button, Image, Select, Table } from "antd";
 import api from "../../../config/axios";
+import { toast } from "react-toastify";
 
 function CareConsign() {
+  const [selectedStatus, setSelectedStatus] = useState({}); // Track staff assignments by order ID
   const [koiList, setKoiList] = useState([]);
   const fetchKoi = async () => {
     const res = await api.get("consignment/staff");
     setKoiList(res.data);
+  };
+  const handleEditOrderStatus = async (id) => {
+    console.log(id);
+    const status = String(selectedStatus.value);
+    try {
+      const res = await api.put(
+        `consignment/status?consignmentId=${id}&newStatus=${status}`
+      );
+      console.log(res.data);
+      fetchKoi();
+    } catch (err) {
+      toast.error(err);
+    }
   };
   useEffect(() => {
     fetchKoi();
@@ -93,6 +108,57 @@ function CareConsign() {
       title: "Care Type",
       dataIndex: "careTypeName",
       key: "careTypeName",
+    },
+    {
+      title: "Update Status",
+      dataIndex: "consignmentID",
+      key: "consignmentID",
+      render: () => (
+        <Select
+          style={{ width: "200px", color: "#000" }}
+          onChange={(value) =>
+            setSelectedStatus((prev) => ({ ...prev, value }))
+          }
+        >
+          <Select.Option style={{ color: "#000" }} value="PENDING">
+            PENDING
+          </Select.Option>
+          <Select.Option style={{ color: "#000" }} value="CONFIRMED">
+            CONFIRMED
+          </Select.Option>
+          <Select.Option style={{ color: "#000" }} value="PAID">
+            PAID
+          </Select.Option>
+          <Select.Option style={{ color: "#000" }} value="CANCELLED">
+            CANCELLED
+          </Select.Option>
+          <Select.Option style={{ color: "#000" }} value="DECLINED">
+            DECLINED
+          </Select.Option>
+          <Select.Option style={{ color: "#000" }} value="SHIPPED">
+            SHIPPED
+          </Select.Option>
+          <Select.Option style={{ color: "#000" }} value="COMPLETED">
+            COMPLETED
+          </Select.Option>
+        </Select>
+      ),
+    },
+    {
+      title: "Action",
+      dataIndex: "consignmentID",
+      key: "consignmentID",
+      render: (id) => (
+        <>
+          <Button
+            onClick={() => {
+              handleEditOrderStatus(id);
+            }}
+          >
+            Confirm
+          </Button>
+        </>
+      ),
     },
   ];
   return <Table columns={columns} dataSource={koiList}></Table>;
